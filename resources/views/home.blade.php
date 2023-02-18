bcb55fc8f3e2c59cc43a31a17d3e08419d6b9534
lollomrt <lorenzomartini.mrt@outlook.it>
2023-02-18 13:45:53 +0100

    Header e Footer creati - creazione layout app.blade.php, inclusione header e footer e yeld('content')

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        @vite('resources/js/app.js')
-
-    </head>
-    <body>
-        @include('partials.header')
-        <main class="back-dark py-5">
-            <div class="container-cards">
-                <div class="row d-flex gap-5 flex-column align-items-center">
-                    <div class="col d-flex flex-wrap gap-1-rem w-100">
-                        @foreach($comics as $comic)
-                            <div class="card card-styled border-0 back-dark text-white">
-                                <img src="{{ $comic['thumb']}}" alt="">
-                                <h6 class="my-2">{{ $comic['title']}}</h6>
-                            </div>
-                        @endforeach
-                    </div>
-                    <button class="btn btn-primary w-25">ihbihbihb</button>
+@section('content')
+    <main class="back-dark py-5">
+        <div class="container-cards">
+            <div class="row d-flex gap-5 flex-column align-items-center">
+                <div class="col d-flex flex-wrap gap-1-rem w-100">
+                    @foreach($comics as $comic)
+                        <div class="card card-styled border-0 back-dark text-white">
+                            <img src="{{ $comic['thumb']}}" alt="">
+                            <h6 class="my-2">{{ $comic['title']}}</h6>
+                        </div>
+                    @endforeach
                 </div>
+                <button class="btn btn-primary w-25">ihbihbihb</button>
             </div>
-        </main>
-    </body>
-</html>
+        </div>
+    </main>
+@endsection
